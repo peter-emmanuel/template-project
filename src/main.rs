7836549f1efc23c591b823286d180e19d1f3e8bc
peter-emmanuel <peter.1.emmanuel@gmail.com>
2024-11-19 15:06:05 -0800

    removed the average function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn type_of<T: std::fmt::Debug>(s: &str, _:&T) -> &'static str {
+fn type_of<T: std::fmt::Debug>(s: &str, _: &T) -> &'static str {
     // Get the type name of the generic parameter T and print it along with the string s
     let tn = std::any::type_name::<T>();
     println!("{s} has type of {tn}");
@@ -11,15 +11,9 @@ fn option_type<T: std::fmt::Display>(opt: &Option<T>) {
         Some(v) => {
             let tn = std::any::type_name::<T>();
             println!("{tn} has value of <{v}>");
-        },
+        }
         None => println!("None"),
     }
-}
-
-fn average(numbers: &[i32]) -> f64 {
-    // Sum the numbers and divide by the length to get the average
-    let total = numbers.iter().sum::<i32>() as f64; // Sum all elements in the array
-    total / numbers.len() as f64 // Divide the total by the number of elements to get the average
 }
 
 fn main() {
@@ -30,12 +24,4 @@ fn main() {
 
     let opt = Some(35.09f64);
     option_type(&opt);
-
-    let numbers = [1, 2, 3, 4, 5];
-    let avg = average(&numbers);
-
-    // Print the list of numbers and their average
-    println!("Numbers in list are {:?}", numbers);
-    println!("Average numbers from list is {avg}");
 }
-
